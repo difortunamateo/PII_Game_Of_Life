51bce2e3bf5c06a76658199a7516dcf6fe83a16f
eugeniaguibernau <eugenia.guibernau@correo.ucu.edu.uy>
2024-08-28 10:12:04 -0300

    Arreglo 3

--- src/Program/Board.cs
+++ src/Program/Board.cs
@@ -2,26 +2,23 @@ namespace Ucu.Poo.GameOfLife;
 
 public class Board
 {
-    
+    //constructor
+    Board(int LengthBoard, int WidthBoard)
+    {
+        this.cellState = new bool[WidthBoard, LengthBoard];
+    }
     
     // Lista de booleanos con el estado del tablero
     private bool[,] cellState;
-
-    public bool[,] CellState
-    {
-        get { return cellState; }
-
-        set { this.cellState= value; }
-    }
-
+    
     public int GetLength(int num)
     {
-        return CellState.GetLength(num);
+        return cellState.GetLength(num);
     }
 
     public bool GetCellValue(int i, int j)
     {
-        return CellState[i, j];
+        return cellState[i, j];
     }
 
     public void SetCellValue(int i, int j, bool value)
