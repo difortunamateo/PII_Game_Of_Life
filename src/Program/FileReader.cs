b38dc750263bab172898bdc075a2e5a2328368e1
Your Name <you@example.com>
2024-08-29 18:00:12 -0300

    changes

--- src/Program/FileReader.cs
+++ src/Program/FileReader.cs
@@ -11,18 +11,14 @@ public class FileReader
         string content = File.ReadAllText(FilePath);
         string[] contentLines = content.Split('\n');
         Board board = new Board(contentLines.Length, contentLines[0].Length);
-
-        for (int y = 0; y < contentLines.Length; y++)
+            
+        for (int y=0; y<contentLines.Length; y++)
         {
-            for (int x = 0; x < contentLines[y].Length; x++)
+            for (int x=0; x<contentLines[y].Length; x++)
             {
-                if (contentLines[y][x] == '1')
+                if(contentLines[y][x]=='1')
                 {
-                    board.SetCellValue(x, y, true);
-                }
-                else
-                {
-                    board.SetCellValue(x, y, false);
+                    board.SetCellValue(x,y, true);
                 }
             }
         }
